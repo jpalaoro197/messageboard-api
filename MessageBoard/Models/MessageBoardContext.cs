96b36c871dcfae587e5d111564045a12d9c3c460
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-18 10:15:55 -0700

    change seed data.

--- MessageBoard/Models/MessageBoardContext.cs
+++ MessageBoard/Models/MessageBoardContext.cs
@@ -18,11 +18,11 @@ namespace MessageBoard.Models
     protected override void OnModelCreating(ModelBuilder builder)
     {
       builder.Entity<Topic>().HasData(
-        new Topic { TopicId = 1, Title = "Coding", Description = "Computer chat for big nerds." },
-        new Topic { TopicId = 2, Title = "Gamin'", Description = "Talking about gaming... lol"},
-        new Topic { TopicId = 3, Title = "Movies", Description = "Motion Picture Appreciators"},
-        new Topic { TopicId = 4, Title = "Sports", Description = "SporsBall... americas passtime"},
-        new Topic { TopicId = 5, Title = "Misc", Description = "Anything goes"}
+        new Topic { TopicId = 1, Title = "mxgp", Description = "word motocross championship." },
+        new Topic { TopicId = 5, Title = "wsx", Description = "world supercross championship"}
+        new Topic { TopicId = 2, Title = "motocross'", Description = "American motocross championship"},
+        new Topic { TopicId = 3, Title = "supercross", Description = "American supercross championship"},
+        new Topic { TopicId = 4, Title = "fmx", Description = "freestyle freaks"},
       );
     }
   }
